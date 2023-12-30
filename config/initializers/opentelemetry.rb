b6743b1b4b5bb25f5e3675720f47c649e9baae1c
AA <>
2023-12-30 22:59:31 +0000

    Edit opentelemetry config to record more data

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,24 +1,43 @@
 require 'opentelemetry/sdk'
 require 'opentelemetry/exporter/jaeger'
-require 'opentelemetry/instrumentation/rails'
-require 'opentelemetry/instrumentation/rack'
+require 'opentelemetry/instrumentation/all'
 
 OpenTelemetry::SDK.configure do |c|
   c.service_name = 'Mastodon'
   c.service_version = '4.2.3'
 
-  # c.use 'OpenTelemetry::Instrumentation::ActionPack'
-  # c.use 'OpenTelemetry::Instrumentation::ActionView'
-  # c.use 'OpenTelemetry::Instrumentation::ActiveJob'
-  # c.use 'OpenTelemetry::Instrumentation::ActiveModelSerializers'
-  # c.use 'OpenTelemetry::Instrumentation::ActiveRecord'
-  # c.use 'OpenTelemetry::Instrumentation::Rails'
-  # c.use 'OpenTelemetry::Instrumentation::Rack', {
-  #   allowed_request_headers: :all,
-  #   allowed_response_headers: :all
-  # }
+  config = {
+    'OpenTelemetry::Instrumentation::Rack' => {
+      record_frontend_span: true,
+      allowed_request_headers: :all,
+      allowed_response_headers: :all
+    },
+    'OpenTelemetry::Instrumentation::ActiveRecord' => {
+      enable_sql_obfuscation: false,
+    },
+    'OpenTelemetry::Instrumentation::Redis' => {
+      capture_arguments: true,
+      db_statement: :include
+    },
+    'OpenTelemetry::Instrumentation::Postgres' => {
+      enable_sql_obfuscation: false,
+      db_statement: :include
+    },
+    'OpenTelemetry::Instrumentation::Sidekiq' => {
+      span_naming: :job_class,
+      propagation_style: :link
+    },
+    'OpenTelemetry::Instrumentation::Excon' => {
+      # List of hosts to be excluded from otel tracing
+      untraced_hosts: [],
+      peer_services: 'NameOfExternalService'
+    },
+    'OpenTelemetry::Instrumentation::Faraday' => {
+      peer_service: 'NameOfExternalService'
+    }
+  }
 
-  c.use_all
+  c.use_all(config)
 
   c.add_span_processor(
     OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
